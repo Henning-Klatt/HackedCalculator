e3d129feac2824329643e96b8832b8c98a3b2494
Henning Klatt <henning.klatt@web.de>
2017-01-25 17:07:48 +0100

    First Line

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 # coding: utf8
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
 
 import Adafruit_ILI9341 as TFT
 import Adafruit_GPIO as GPIO
@@ -17,6 +14,5 @@ SPI_DEVICE = 0
 
 disp = TFT.ILI9341(DC, rst=RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=64000000))
 disp.begin()
-disp.clear()
 
 Manager.startScreen(disp)
--- screenmanager.py
+++ screenmanager.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # coding: utf8
-
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 class Manager:
     @classmethod
     def startScreen(self, disp):
-        disp.clear((0, 255, 0))
+        disp.clear((0, 0, 0))
+        draw = disp.draw()
+        draw.line((10, 170, 110, 230), fill=(255,255,255))
+        disp.display()
 
 
 def main():
