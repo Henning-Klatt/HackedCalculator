41e893de4af6172ca10a5c107d8be3c52e2f584f
Henning Klatt <henning.klatt@web.de>
2017-01-25 16:49:31 +0100

    Global var

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ RST = 23
 SPI_PORT = 0
 SPI_DEVICE = 0
 
-disp = TFT.ILI9341(DC, rst=RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=64000000))
 global disp
+disp = TFT.ILI9341(DC, rst=RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=64000000))
 disp.begin()
 disp.clear()
 
