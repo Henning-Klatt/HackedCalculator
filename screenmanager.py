f37f29952b47bdfe43a9d40dd712fec57c48218d
Henning Klatt <henning.klatt@web.de>
2017-01-25 17:19:43 +0100

    Debug

--- screenmanager.py
+++ screenmanager.py
@@ -9,7 +9,8 @@ class Manager:
     def startScreen(self, disp):
         disp.clear((0, 0, 0))
         draw = disp.draw()
-        draw.line((0, 320, 1, 321), fill=(255,255,255))
+        #
+        draw.line((10, 170, 110, 230), fill=(255,255,255))
         draw_text(disp.buffer, "Henning's hacked calculator!", (50, 150), 90, ImageFont.load_default(), fill=(255,255,255))
         disp.display()
 
