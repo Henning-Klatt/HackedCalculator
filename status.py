eef3f24764600b258d24fcca152fa73b90ae03f6
Henning Klatt <henning.klatt@web.de>
2017-01-25 18:59:15 +0100

    Fix

--- status.py
+++ status.py
@@ -23,7 +23,7 @@ def run(disp):
         draw = disp.draw()
         uptime = str(timedelta(seconds = float(open('/proc/uptime', 'r').readline().split()[0]))).rsplit('.', 1)[0]
         cpu_usage = str(psutil.cpu_percent())
-        screenmanager.draw_text(disp.buffer, "Uptime: " + uptime, (0, 220), 90, font_clean, fill=(255,255,255))
-        screenmanager.draw_text(disp.buffer, "CPU Load: " + cpu_usage, (20, 220), 90, font_clean, fill=(255,255,255))
+        screenmanager.draw_text(disp.buffer, "Uptime: " + uptime, (0, 220), 270, font_clean, fill=(255,255,255))
+        screenmanager.draw_text(disp.buffer, "CPU Load: " + cpu_usage + " %", (20, 220), 90, font_clean, fill=(255,255,255))
         disp.display()
         time.sleep(1)
