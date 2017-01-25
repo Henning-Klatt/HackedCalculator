ed47d80d85a69aeadae8ec9a8881add02995e001
Henning Klatt <henning.klatt@web.de>
2017-01-25 17:04:03 +0100

    Added disp argument

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
 # coding: utf8
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
-import Adafruit_ILI9341 as TFT
-import Adafruit_GPIO as GPIO
-import Adafruit_GPIO.SPI as SPI
 
 from screenmanager import Manager
 
@@ -19,4 +12,4 @@ disp = TFT.ILI9341(DC, rst=RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_h
 disp.begin()
 disp.clear()
 
-Manager.startScreen()
+Manager.startScreen(disp)
--- screenmanager.py
+++ screenmanager.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 # coding: utf8
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import Adafruit_ILI9341 as TFT
+import Adafruit_GPIO as GPIO
+import Adafruit_GPIO.SPI as SPI
 
 class Manager:
     @classmethod
-    def startScreen(self):
+    def startScreen(self, disp):
         disp.clear((0, 255, 0))
 
 
 def main():
     print "Main Funktion"
+
 
 if __name__ == '__main__':
     main()
