f41f1d1412c2f45cf1a1044337fed1511d118e6f
Henning Klatt <henning.klatt@web.de>
2017-01-25 18:35:07 +0100

    Fix

--- status.py
+++ status.py
@@ -9,6 +9,8 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+from screenmanager import draw_text
+
 class Status(object):
     def start(self, disp):
         print "Statusseite gestartet"
@@ -21,6 +23,6 @@ def run(disp):
         draw = disp.draw()
         font = ImageFont.truetype('Fonts/clean.ttf', 11)
         uptime = str(timedelta(seconds = float(open('/proc/uptime', 'r').readline().split()[0]))).rsplit('.', 1)[0]
-        draw_text(disp.buffer, "Uptime: " + uptime, (20, 0), 90, font, fill=(255,255,255))
+        screenmanager.draw_text(disp.buffer, "Uptime: " + uptime, (20, 0), 90, font, fill=(255,255,255))
         disp.display()
         time.sleep(1)
