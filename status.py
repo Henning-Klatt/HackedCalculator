aa4cc984db26636f6c65123f7ec5a8a20f5e3091
Henning Klatt <henning.klatt@web.de>
2017-01-25 17:43:18 +0100

    Fix

--- status.py
+++ status.py
@@ -8,15 +8,15 @@ class Status(object):
     def __init__(self, interval=1):
         self.interval = interval
         thread = threading.Thread(target=self.run, args=())
-        thread.daemon = True                            # Daemonize thread
-        thread.start()                                  # Start the execution
+        thread.daemon = True 
+        thread.start()
 
     def run(self):
         while True:
             print('Doing something imporant in the background')
             time.sleep(self.interval)
 
-example = Status()
+#example = Status()
 time.sleep(3)
 print('Checkpoint')
 time.sleep(2)
