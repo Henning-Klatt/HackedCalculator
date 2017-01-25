cc884c3784606590411a14e70237b7a9be665a58
Henning Klatt <henning.klatt@web.de>
2017-01-25 16:38:49 +0100

    Black screen

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python
 # coding: utf8
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+
+import Adafruit_ILI9341 as TFT
+import Adafruit_GPIO as GPIO
+import Adafruit_GPIO.SPI as SPI
+
+DC = 18
+RST = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+disp = TFT.ILI9341(DC, rst=RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=64000000))
+disp.begin()
+disp.clear()
