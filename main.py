1edef2bc1393e13eccd516ed1ee38c2f629f48cb
Henning Klatt <henning.klatt@web.de>
2017-01-25 16:43:33 +0100

    Added startScreen

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-
 import Adafruit_ILI9341 as TFT
 import Adafruit_GPIO as GPIO
 import Adafruit_GPIO.SPI as SPI
+
+import startscreen
 
 DC = 18
 RST = 23
@@ -17,3 +18,5 @@ SPI_DEVICE = 0
 disp = TFT.ILI9341(DC, rst=RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=64000000))
 disp.begin()
 disp.clear()
+
+startScreen()
---
+++ startscreen.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# coding: utf8
+
+def startScreen():
+    disp.clear((0, 255, 0))
