9663c618a9d350246e03cbd86cbd1c4142ec15ed
Henning Klatt <henning.klatt@web.de>
2017-01-25 18:56:21 +0100

    Added CPU Usage

--- status.py
+++ status.py
@@ -21,6 +21,8 @@ def run(disp):
         disp.clear((0, 0, 0))
         draw = disp.draw()
         uptime = str(timedelta(seconds = float(open('/proc/uptime', 'r').readline().split()[0]))).rsplit('.', 1)[0]
+        cpu_usage = psutil.cpu_percent()
         screenmanager.draw_text(disp.buffer, "Uptime: " + uptime, (0, 220), 90, font_clean, fill=(255,255,255))
+        screenmanager.draw_text(disp.buffer, "CPU Load: " + cpu_usage, (20, 220), 90, font_clean, fill=(255,255,255))
         disp.display()
         time.sleep(1)
