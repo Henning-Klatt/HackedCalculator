ec4f29652c36801fd6e6bea7f2700661dee9bdc6
Henning Klatt <henning.klatt@web.de>
2017-01-25 18:00:38 +0100

    Debug

--- screenmanager.py
+++ screenmanager.py
@@ -17,7 +17,9 @@ class Manager:
         font_hacked = ImageFont.truetype('Fonts/hacked.ttf', 25)
         draw_text(disp.buffer, "Henning's hacked calculator!", (50, 0), 90, font_hacked, fill=(255,255,255))
         disp.display()
-        Status.start()
+        statusmanager = Status()
+        statusmanager.start()
+
 
 def draw_text(image, text, position, angle, font, fill=(255,255,255)):
     draw = ImageDraw.Draw(image)
