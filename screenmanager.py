090ea4dfb5379a715d4af213d44178281532e7ab
Henning Klatt <henning.klatt@web.de>
2017-01-26 20:13:22 +0100

    Fix

--- screenmanager.py
+++ screenmanager.py
@@ -25,7 +25,6 @@ class Manager:
         draw_text(disp.buffer, "Henning's hacked calculator!", (45, 0), 90, font_hacked, fill=(255,255,255))
         disp.display()
         time.sleep(2)
-        global statusthread
         statusthread = Thread(target=status.run, args=(disp, statusthread))
         statusthread.start()
 
