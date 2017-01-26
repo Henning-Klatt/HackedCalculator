d9c10199fbe068695a84aff589e7f49fe720fd40
Henning Klatt <henning.klatt@web.de>
2017-01-26 19:23:44 +0100

    Fix

--- keymanager.py
+++ keymanager.py
@@ -5,6 +5,11 @@ import time
 
 def run():
     try:
+        input = raw_input('> ')
+        if(input == "exit"):
+            print "Beende Programm..."
+        else:
+            print input
         while True:
             time.sleep(.1)
     except KeyboardInterrupt:
