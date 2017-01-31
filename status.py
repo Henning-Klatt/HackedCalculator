d59283725cb8605d6c91ab33c5b7c71a53bfb461
Henning Klatt <henning.klatt@web.de>
2017-01-31 19:36:37 +0100

    More threading

--- status.py
+++ status.py
@@ -12,15 +12,15 @@ from fonts import font_clean
 from display import disp
 
 class Status:
-  def run(self):
-    while True:
-        disp.clear((0, 0, 0))
-        draw = disp.draw()
-        uptime = str(timedelta(seconds = float(open('/proc/uptime', 'r').readline().split()[0]))).rsplit('.', 1)[0]
-        cpu_usage = str(psutil.cpu_percent())
-        screenmanager.draw_text(disp.buffer, "Uptime: ", (0, 250), 90, font_clean, fill=(255,255,255))
-        screenmanager.draw_text(disp.buffer, uptime, (0, 200), 90, font_clean, fill=(255,255,255))
-        screenmanager.draw_text(disp.buffer, "CPU Load: ", (20, 250), 90, font_clean, fill=(255,255,255))
-        screenmanager.draw_text(disp.buffer, cpu_usage + " %", (20, 200), 90, font_clean, fill=(255,255,255))
-        disp.display()
-        time.sleep(1)
+    def run(self):
+        while True:
+            disp.clear((0, 0, 0))
+            draw = disp.draw()
+            uptime = str(timedelta(seconds = float(open('/proc/uptime', 'r').readline().split()[0]))).rsplit('.', 1)[0]
+            cpu_usage = str(psutil.cpu_percent())
+            screenmanager.draw_text(disp.buffer, "Uptime: ", (0, 250), 90, font_clean, fill=(255,255,255))
+            screenmanager.draw_text(disp.buffer, uptime, (0, 200), 90, font_clean, fill=(255,255,255))
+            screenmanager.draw_text(disp.buffer, "CPU Load: ", (20, 250), 90, font_clean, fill=(255,255,255))
+            screenmanager.draw_text(disp.buffer, cpu_usage + " %", (20, 200), 90, font_clean, fill=(255,255,255))
+            disp.display()
+            time.sleep(1)
