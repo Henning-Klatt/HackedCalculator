99334a0003616768b50c1c57b2d9eeac037b4ce7
Henning Klatt <henning.klatt@web.de>
2017-03-12 09:00:41 +0100

    Fixed function

--- status.py
+++ status.py
@@ -8,18 +8,18 @@ import psutil
 import socket
 from datetime import timedelta
 import RPi.GPIO as GPIO
+import screenmanager
+from fonts import font_clean
+from display import disp
+
+def lowBat(channel):
+    print "Low battery!"
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN) #, pull_up_down=GPIO.PUD_DOWN
 GPIO.add_event_detect(4, GPIO.FALLING, callback=lowBat, bouncetime=200)
 
-import screenmanager
-from fonts import font_clean
-from display import disp
 
-
-def lowBat(channel):
-    print "Low battery!"
 
 class Status:
     def run(self):
