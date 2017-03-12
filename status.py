55d8c4b47dcce5a627291499ccdb50c31bc278ff
Henning Klatt <henning.klatt@web.de>
2017-03-12 14:28:00 +0100

    Debug

--- status.py
+++ status.py
@@ -23,16 +23,16 @@ GPIO.add_event_detect(4, GPIO.FALLING, callback=lowBat, bouncetime=200)
 class Status:
     def run(self):
         while True:
-            #disp.reset()
+            disp.reset()
             disp.clear()
             #disp.display()
             draw = disp.draw()
-            uptime = "12:03:23"#str(timedelta(seconds = float(open('/proc/uptime', 'r').readline().split()[0]))).rsplit('.', 1)[0]
-            cpu_usage = "23,7%"#str(psutil.cpu_percent())
-            #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            #s.connect(('192.168.178.1', 1027))
-            ip = "127.0.0.1"#s.getsockname()[0]
-            #s.close()
+            uptime = str(timedelta(seconds = float(open('/proc/uptime', 'r').readline().split()[0]))).rsplit('.', 1)[0]
+            cpu_usage = str(psutil.cpu_percent())
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(('192.168.178.1', 1027))
+            ip = s.getsockname()[0]
+            s.close()
             screenmanager.draw_text(disp.buffer, "Uptime: ", (0, 250), 90, font_clean, fill=(255,255,255))
             screenmanager.draw_text(disp.buffer, uptime, (0, 200), 90, font_clean, fill=(255,255,255))
             screenmanager.draw_text(disp.buffer, "CPU Load: ", (20, 250), 90, font_clean, fill=(255,255,255))
