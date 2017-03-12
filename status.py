974a41eac0211dcbe79d2cec2fbae9dfde35954e
Henning Klatt <henning.klatt@web.de>
2017-03-12 15:20:48 +0100

    Fixed Syntax

--- status.py
+++ status.py
@@ -40,7 +40,7 @@ class Status:
             screenmanager.draw_text(disp.buffer, uptime, (0, 200), 90, font_clean, fill=(255,255,255))
             screenmanager.draw_text(disp.buffer, "CPU Load: ", (20, 250), 90, font_clean, fill=(255,255,255))
             screenmanager.draw_text(disp.buffer, cpu_usage + " %", (20, 200), 90, font_clean, fill=(255,255,255))
-            screenmanager.draw_text(disp.buffer, "Free RAM: ",40, 250), 90, font_clean, fill=(255,255,255))
+            screenmanager.draw_text(disp.buffer, "Free RAM: ", (40, 250), 90, font_clean, fill=(255,255,255))
             screenmanager.draw_text(disp.buffer, free_ram, (40), 50), 90, font_clean, fill=(255,255,255))
             screenmanager.draw_text(disp.buffer, "IP: ", (60, 250), 90, font_clean, fill=(255,255,255))
             screenmanager.draw_text(disp.buffer, ip, (60, 50), 90, font_clean, fill=(255,255,255))
