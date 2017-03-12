0a20f68ae718b74b5dd7c99cf06ae97028ea57b8
Henning Klatt <henning.klatt@web.de>
2017-03-12 15:20:13 +0100

    RAM managmend

--- status.py
+++ status.py
@@ -23,9 +23,12 @@ GPIO.add_event_detect(4, GPIO.FALLING, callback=lowBat, bouncetime=200)
 class Status:
     def run(self):
         while True:
-            disp.reset()
-            disp.begin()
-            #disp.clear()
+            free_ram = psutil.virtual_memory()[1]
+            print free_ram
+            #if((free_ram) < 10000000):
+                #disp.reset()
+                #disp.begin()
+            disp.clear()
             draw = disp.draw()
             uptime = str(timedelta(seconds = float(open('/proc/uptime', 'r').readline().split()[0]))).rsplit('.', 1)[0]
             cpu_usage = str(psutil.cpu_percent())
@@ -37,9 +40,11 @@ class Status:
             screenmanager.draw_text(disp.buffer, uptime, (0, 200), 90, font_clean, fill=(255,255,255))
             screenmanager.draw_text(disp.buffer, "CPU Load: ", (20, 250), 90, font_clean, fill=(255,255,255))
             screenmanager.draw_text(disp.buffer, cpu_usage + " %", (20, 200), 90, font_clean, fill=(255,255,255))
-            screenmanager.draw_text(disp.buffer, "IP: ", (40, 250), 90, font_clean, fill=(255,255,255))
-            screenmanager.draw_text(disp.buffer, ip, (40, 50), 90, font_clean, fill=(255,255,255))
-            screenmanager.draw_text(disp.buffer, "BT: ", (60, 250), 90, font_clean, fill=(255,255,255))
-            screenmanager.draw_text(disp.buffer, str(GPIO.input(4)), (60, 200), 90, font_clean, fill=(255,0,0))
+            screenmanager.draw_text(disp.buffer, "Free RAM: ",40, 250), 90, font_clean, fill=(255,255,255))
+            screenmanager.draw_text(disp.buffer, free_ram, (40), 50), 90, font_clean, fill=(255,255,255))
+            screenmanager.draw_text(disp.buffer, "IP: ", (60, 250), 90, font_clean, fill=(255,255,255))
+            screenmanager.draw_text(disp.buffer, ip, (60, 50), 90, font_clean, fill=(255,255,255))
+            screenmanager.draw_text(disp.buffer, "BT: ", (80, 250), 90, font_clean, fill=(255,255,255))
+            screenmanager.draw_text(disp.buffer, str(GPIO.input(4)), (80, 200), 90, font_clean, fill=(255,0,0))
             disp.display()
             time.sleep(1)
