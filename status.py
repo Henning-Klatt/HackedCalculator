896c0ebbdd46362949a00dbaea82685de5abbd9d
Henning Klatt <henning.klatt@web.de>
2017-03-12 09:12:26 +0100

    Fixed an overlapping design

--- status.py
+++ status.py
@@ -36,7 +36,7 @@ class Status:
             screenmanager.draw_text(disp.buffer, "CPU Load: ", (20, 250), 90, font_clean, fill=(255,255,255))
             screenmanager.draw_text(disp.buffer, cpu_usage + " %", (20, 200), 90, font_clean, fill=(255,255,255))
             screenmanager.draw_text(disp.buffer, "IP: ", (40, 250), 90, font_clean, fill=(255,255,255))
-            screenmanager.draw_text(disp.buffer, ip, (40, 200), 90, font_clean, fill=(255,255,255))
+            screenmanager.draw_text(disp.buffer, ip, (40, 50), 90, font_clean, fill=(255,255,255))
             screenmanager.draw_text(disp.buffer, "BT: ", (60, 250), 90, font_clean, fill=(255,255,255))
             screenmanager.draw_text(disp.buffer, str(GPIO.input(4)), (60, 200), 90, font_clean, fill=(255,0,0))
             disp.display()
